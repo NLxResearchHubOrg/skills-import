42efdf673d0d99324ebb38e05759fedcdc4aaa29
Ripp <31484824+loganripplinger@users.noreply.github.com>
2020-03-23 08:00:20 -0700

    Update sync_nlx.py

--- sync_nlx.py
+++ sync_nlx.py
@@ -13,9 +13,8 @@ import os.path
 
 filehandles = {}
 writers = {}
+s3_keys = {}
 start_time = datetime.now()
-database = "" # Do not modify here, modify in run function
-
 
 def print_elapsed_time():
     global start_time
@@ -25,9 +24,8 @@ def print_elapsed_time():
     mins = divmod(duration_in_s, 60)[0]
     print('Elapsed: {} hours / {} mins'.format(hours, mins))
 
-
-def filename(quarter_string):
-    return 'C:/Output/1/{}.csv'.format(quarter_string)
+def filename(quarter_string): # TODO create folder if not exist
+    return 'C:/Output/1/{}_sanitized_oct2015.csv'.format(quarter_string)
 
 
 def output_writer(row, cursor_desc):
@@ -54,7 +52,7 @@ def output_writer(row, cursor_desc):
     return writers[year_string]
 
 
-QUERY = """
+SANITIZED_QUERY = """
 SELECT
 j.generatedJobId,
 [jobID],
@@ -68,11 +66,10 @@ j.country,
 [expired],
 [expiryDate],
 [fedcontractor],
-[fein],
 [lastUpdatedDate],
-[link],
 [sourceState],
 [title],
+[fileName],
 onet.value as onet_code,
 naics.value as naics_code,
 mined.value as min_education,
@@ -86,13 +83,11 @@ shift.maximum as shift,
 salary.minimum as minSalary,
 salary.maximum as maxSalary,
 salary.unit as salaryUnit,
-a.company,
 addr.city as companyCity,
 addr.state as companyState,
 addr.zipcode as companyZipcode,
-addr.country as companyCountry,
-addr.line1 as companyAddress
-FROM [{database_name}].[dbo].[Job] j
+addr.country as companyCountry
+FROM [LMI-Oct2015].[dbo].[Job] j
 LEFT JOIN Classification onet ON (
     j.generatedJobId = onet.generatedJobId and onet.type = 'ONET'
 )
@@ -132,40 +127,118 @@ LEFT JOIN Parameter salary on (
 )
 LEFT JOIN [Application] a on (j.generatedJobId = a.generatedJobId)
 LEFT JOIN [Address] addr on (a.applicationId = addr.applicationId)
-WHERE j.generatedJobId >= {min_pk} and j.generatedJobId < {max_pk}
+WHERE [dateacquired] BETWEEN Convert(datetime, '2017-01-01' ) AND Convert(datetime, '2018-12-31 23:59:59')
+AND j.generatedJobId >= {min_pk} and j.generatedJobId < {max_pk}
+"""
+
+UNSANITIZED_QUERY = """
+SELECT
+j.generatedJobId,
+[jobID],
+j.city,
+j.state,
+j.zipcode,
+j.country,
+[createddate],
+[dateacquired],
+[description],
+[expired],
+[expiryDate],
+[fedcontractor],
+[fein],
+[lastUpdatedDate],
+[link],
+[sourceState],
+[title],
+onet.value as onet_code,
+naics.value as naics_code,
+mined.value as min_education,
+expr.value as experience,
+lic.value as license,
+train.value as training,
+positions.maximum as maxPositions,
+duration.maximum as jobDuration,
+hoursperweek.maximum as weeklyHours,
+shift.maximum as shift,
+salary.minimum as minSalary,
+salary.maximum as maxSalary,
+salary.unit as salaryUnit,
+a.company,
+addr.city as companyCity,
+addr.state as companyState,
+addr.zipcode as companyZipcode,
+addr.country as companyCountry,
+addr.line1 as companyAddress
+FROM [LMI-April2018].[dbo].[Job] j
+LEFT JOIN Classification onet ON (
+    j.generatedJobId = onet.generatedJobId and onet.type = 'ONET'
+)
+LEFT JOIN Classification naics ON (
+    j.generatedJobId = naics.generatedJobId and naics.type = 'NAICS'
+)
+LEFT JOIN Requirement mined on (
+    j.generatedJobId = mined.generatedJobId and mined.type = 'mineducation'
+)
+LEFT JOIN Requirement expr on (
+    j.generatedJobId = expr.generatedJobId and expr.type = 'experience'
+)
+LEFT JOIN Requirement lic on (
+    j.generatedJobId = lic.generatedJobId and lic.type = 'license'
+)
+LEFT JOIN Requirement train on (
+    j.generatedJobId = train.generatedJobId and train.type = 'training'
+)
+LEFT JOIN Parameter positions on (
+    j.generatedJobId = positions.generatedJobId and parameterType = 'positions'
+)
+LEFT JOIN Parameter duration on (
+    j.generatedJobId = duration.generatedJobId
+    and duration.parameterType = 'duration'
+)
+LEFT JOIN Parameter hoursperweek on (
+    j.generatedJobId = hoursperweek.generatedJobId
+    and hoursperweek.parameterType = 'hoursperweek'
+)
+LEFT JOIN Parameter shift on (
+    j.generatedJobId = shift.generatedJobId
+    and shift.parameterType = 'shift'
+)
+LEFT JOIN Parameter salary on (
+    j.generatedJobId = salary.generatedJobId
+    and salary.parameterType = 'salary'
+)
+LEFT JOIN [Application] a on (j.generatedJobId = a.generatedJobId)
+LEFT JOIN [Address] addr on (a.applicationId = addr.applicationId)
+WHERE [dateacquired] BETWEEN Convert(datetime, '2020-01-01' ) AND Convert(datetime, '2020-12-31 23:59:59')
+AND j.generatedJobId >= {min_pk} and j.generatedJobId < {max_pk}
 """
 
 
 def row_to_dict(row):
     return dict(zip([t[0] for t in row.cursor_description], row))
 
-# These global variables will attempt to restore the query state
-# if this encounters a disconnect
-i = 0  
+i = 0
 total = 0
 
-
 def get_batch(cursor, min_pk, batch_size):
-    nonlocal database
-    bound_query = QUERY.format(
-        database_name=database,
-        min_pk=min_pk,
-        max_pk=min_pk+batch_size)
+    bound_query = SANITIZED_QUERY.format(min_pk=min_pk, max_pk=min_pk+batch_size) # TODO add db
     cursor.execute(bound_query)
     cursor_desc = dict(
         (field[0], field[0])
         for field in cursor.description
     )
 
-    # Restore cursor state if we dropped connection
     global i
     global total
+    #items = cursor.fetchall()
+    #print(items)
     temp = i
     while temp != 0:
         cursor.fetchone()
         temp-=1
         
-    for row in cursor:
+    for row in cursor: # this is what takes forever
+        #print(row)
         i += 1
         total += 1
         if i % 100000 == 0:
@@ -188,20 +261,33 @@ def close_files():
 def run():
     max_pk = 52602456
     batch_size = 1000000
+    #batch_size = 10
     disconnect = False
     for min_pk in range(0, max_pk, batch_size):
         while True:
             try:
-                print('Time: ', str(datetime.now()))
+                print('yea top of try')
+                print('now: ', str(datetime.now()))
                 print_elapsed_time()
-                nonlocal database
+                if disconnect:
+                    #conn = pymssql.connect(server="EC2AMAZ-VG341JF")
+                    #time.sleep(60)
+                    pass
                     
-                server = 'EC2AMAZ-U6JE5SD'
-                database = 'LMI'
-                username = 'pytest' 
-                password = '' 
+                #conn = pymssql.connect(server="EC2AMAZ-VG341JF")
+                server = 'EC2AMAZ-AAAAA' 
+                database = 'LMI-Oct2015' 
+                username = '' 
+                password = ''
                 connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
+                # connection = pymssql.connect(
+                    # server='EC2AMAZ-VG341JF',
+                    # user='pytest',
+                    # password='',
+                    # database='LMI-April2018',
+                    # login_timeout=3
+                # )
                 print('1')
                 cursor = connection.cursor()
                 print('2')
@@ -209,8 +295,9 @@ def run():
                 print('3')
                 print('Done with min pk', min_pk)
 
+                # close_files()
+                # connection.close()
                 break
-
             except pyodbc.OperationalError as e:
                 print('caught a disconnect')
                 print(e)
@@ -229,5 +316,40 @@ def run():
             
             finally:
                 close_files()
+                
+        #print('Now uploading files to s3')
+        #for year, fh in filehandles.items():
+        #    upload(
+        #        s3_conn,
+        #        filename(year),
+        #        '{}/{}.gz'.format(output_s3_prefix, year)
+        #    )
+    # except _mssql.MssqlDriverException:
+        # print("A MSSQLDriverException has been caught.")
+
+    # except _mssql.MssqlDatabaseException as e:
+        # print("A MSSQLDatabaseException has been caught.")
+        # print('Number = ',e.number)
+        # print('Severity = ',e.severity)
+        # print('State = ',e.state)
+        # print('Message = ',e.message)
+        
+    # except Exception as e:
+        # try:
+            # if int(str(e)[1:6]) == 20047:
+                # print('caught a disconnect')
+        # except ValueError:
+            # pass
+            
+        # raise e
+        
+    # finally:
+        # print('closing files...')
+        # close_files()
+            
+        # print('closed files')
+        # print('closing connection...')
+        # connection.close()
+        # print('connection closed.')
 
 run()
